c1c6ec0b79032ebafffd3461ad16e9dccc778da7
kfabiank <kfabiank@gmail.com>
2017-07-09 15:46:11 -0600

    Test 80% completed

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # prosoftTest
+
+Bugs Identified: 
+
+1.Dates calculation, I need to double check the date calculation given an start day and numbers of days
+2.Date Rage is been calculated but is not working yet in the calendar rendering, with more time I will be able to create an array of days to call the endpoint and return the data I need.
+3.Country code is been passed as parameter to the API buy for example if I pass CR and US is currently returning holidays for US only. With more time I may need to make some test and find more documentation.
+4. Calendar is been generated, however I did not have time to generate only the month requested, so I’m adding arrows to navigate between months.
+
+This that need more doc or requirements,
+
+Country code is been passed as parameter to the API buy for example if I pass CR and US is currently returning holidays for US only. With more time I may need to make some test and find more documentation.
+
+Things Implemented
+A.	Weekends are yellow.
+
+B.	W e e k d a y s a r e gr e e n .
+
+C.	H o l i d a y s a r e or a n g e
+
+
+D.	Invalid days are hidden and the background should be gray.
+E.	4. The Holidays must be dynamically extracted from the API defined at
+F.	http://holidayapi.com holidays are only required for the year 2008.
+G.	This is a RESTful API, for which we expect web requests to be created and handled appropriately.
+
+H.	We also expect different result codes to be handled.
+I.	Extra credit will be given for adding a rollover label on Holidays. Currently this is working as expected, when you rollover the orange circles or the Holidays, it shows the name of the Holiday.
+J.	Form is getting and validating the inputs needed.
--- js/getdata.js
+++ js/getdata.js
@@ -28,3 +28,92 @@ var events1 = [
         "public": false
     }
 ];
+
+
+/*This is a function to calculate future date base on one date + integer number of days*/
+
+function addDates(startDate, noOfDaysToAdd) {
+    var count = 0;
+    while (count < noOfDaysToAdd) {
+        endDate = new Date(startDate.setDate(startDate.getDate() + 1));
+        if (endDate.getDay() != 0 && endDate.getDay() != 6) {
+            //Date.getDay() gives weekday starting from 0(Sunday) to 6(Saturday)
+            count++;
+        }
+    }
+    return startDate;
+}
+
+
+
+
+console.log('evnet1--' + events1);
+let events = [];
+let tempEvents;
+let date;
+let daysToAdd;
+let datesRange;
+
+function callEndpoint(startDate,countryCode) {
+    var inputDate = startDate.split('/', 3);
+    var month = inputDate[0];
+    var day = inputDate[1];
+    var year = inputDate[2];
+    var url = 'https://holidayapi.com/v1/holidays?key=e6d65dce-6edf-41f4-acdb-d7008f75a17d&country='+ countryCode+'&year=' + year + '&month=' + month;// + '&day=' + day + '&upcoming=true';
+
+
+
+    $.getJSON(url, function(data, status, xhr) {
+        //Errors
+        // 400 Something is wrong on your end
+        // 401 Unauthorized(did you remember your API key ? )
+        // 402 Payment required(only historical data available is free)
+        // 403 Forbidden(this API is HTTPS - only)
+        // 429 Rate limit exceeded
+        // 500 OH NOES!!~!Something is wrong on our end
+
+        if (status == "success") {
+            tempEvents = data['holidays'];
+            console.log('--------=' + tempEvents);
+
+            console.log(tempEvents);
+            console.log('evnet2--' + tempEvents);
+            var settings = {};
+            var element = document.getElementById('caleandar');
+            caleandar(element, tempEvents, settings);
+        } else if (status == 400) {
+            alert("Something is wrong on your end");
+        } else if (status == 401) {
+            alert("did you remember your API key?");
+        } else if (status == 402) {
+            alert("only historical data available is free");
+        } else if (status == 403) {
+            alert("this API is HTTPS-only");
+        } else if (status == 500) {
+            alert("Something is wrong on our end");
+        }
+
+    });
+
+
+}
+
+
+
+function validateForm() {
+    var startDate = document.getElementById('startDate').value;
+    var numberDays = document.getElementById('numberDays').value;
+    var countryCode = document.getElementById('countryCode').value;
+    if ((startDate == "") && (numberDays == "") && (countryCode == "")) {
+        alert("Plese check the inputs");
+        return false;
+    } else {
+        date = new Date(startDate);
+        daysToAdd = numberDays;
+        datesRange = date + " -- " + addDates(date, daysToAdd)
+        document.getElementById('dates').innerHTML = datesRange;
+        callEndpoint(startDate,countryCode);
+        return false; // avoid to execute the actual submit of the form.
+    }
+
+}
