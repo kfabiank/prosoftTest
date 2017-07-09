6a619066e8284b19b33fa07876c49899e68021c1
kfabiank <kfabiank@gmail.com>
2017-07-09 14:35:54 -0600

    js folder

---
+++ js/getdata.js
@@ -0,0 +1,30 @@
+var events1 = [
+    /*{
+        'Date': new Date(2017, 6, 7),
+        'Title': 'Doctor appointment at 3:25pm.'
+    }, {
+        'Date': new Date(2017, 6, 18),
+        'Title': 'New Garfield movie comes out!',
+        'Link': 'https://garfield.com'
+    }, {
+        'Date': new Date(2017, 6, 27),
+        'Title': '25 year anniversary',
+        'Link': 'https://www.google.com.au/#q=anniversary+gifts'
+    },*/
+    {
+        "name": "Flag Day",
+        "date": "2017-07-14",
+        "observed": "2017-07-14",
+        "public": false
+    }, {
+        "name": "Father's Day",
+        "date": "2017-07-19",
+        "observed": "2017-07-19",
+        "public": false
+    }, {
+        "name": "Helen Keller Day",
+        "date": "2017-07-27",
+        "observed": "2016-06-27",
+        "public": false
+    }
+];
